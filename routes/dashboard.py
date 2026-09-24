a4f2af010f759aa9fc0534d701979d8dd8083d25
bydatdya237-ai <bydatdya237@gmail.com>
2026-09-25 00:32:40 +0300

    Update dashboard.py

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,1 +1,17 @@
+from flask import Blueprint, render_template, redirect, session, url_for
 
+
+dashboard_bp = Blueprint("dashboard", __name__)
+
+
+@dashboard_bp.route("/dashboard")
+def dashboard():
+    user = session.get("discord_user")
+
+    if not user:
+        return redirect(url_for("auth.login"))
+
+    return render_template(
+        "dashboard.html",
+        user=user
+    )
