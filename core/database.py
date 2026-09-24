a23f6e9b581cb10205112060ed212027ea51dded
bydatdya237-ai <bydatdya237@gmail.com>
2026-09-25 00:09:47 +0300

    Update database.py

--- core/database.py
+++ core/database.py
@@ -1,1 +1,28 @@
+from pymongo import MongoClient
 
+from .config import MONGO_URI
+
+
+# =========================================================
+# MongoDB
+# =========================================================
+
+mongo = MongoClient(MONGO_URI)
+
+db = mongo["discord_bot_db"]
+
+
+# =========================================================
+# Collections
+#
+# مهم:
+# لا نغير أي اسم من أسماء الـ Collections الحالية.
+# =========================================================
+
+commands_collection = db["website_commands"]
+
+guilds_collection = db["website_guilds"]
+
+settings_collection = db["website_command_settings"]
+
+economy_settings_collection = db["economy_settings"]
